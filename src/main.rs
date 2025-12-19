3e8b56e0af33dee43baf39faa3a4d8e18b2d90c7
dangnp <dangnp@vng.com.vn>
2025-12-19 20:25:30 +0700

    First read XML by xml::reader

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
-use std::fs;
+use std::fs::File;
 use std::process::exit;
+use xml::reader::{XmlEvent, EventReader};
 
 fn main() {
     let file_path = "docs.gl/gl2/glAccum.xhtml";
-    let content = fs::read_to_string(file_path).unwrap_or_else(|err| {
+    let file = File::open(file_path).unwrap_or_else(|err| {
         eprintln!("ERROR: could not read file {file_path}: {err}");
         exit(1);
     });
+
+    let er = EventReader::new(file);
+    let mut content = String::new();
+    for event in er.into_iter() {
+        let event = event.unwrap_or_else(|err| {
+            eprintln!("ERROR: could not read next XML event: {err}");
+            exit(1);
+        });
+
+        match event {
+            XmlEvent::Characters(text) => {
+                content.push_str(&text);
+            }
+            _=> {}
+        }
+    }
 
     println!("{content}");
 }
