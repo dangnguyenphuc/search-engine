3e3aed906f13ce30eef9b7d0701a300cec6c17f1
dangnp <dangnp@vng.com.vn>
2025-12-20 13:41:55 +0700

    First read all files in a directory

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,37 @@
-use std::fs::File;
+use std::fs::{File};
+use std::fs;
+use std::path::Path;
+use std::io;
 use std::process::exit;
 use xml::reader::{XmlEvent, EventReader};
 
-fn main() {
-    let file_path = "docs.gl/gl2/glAccum.xhtml";
-    let file = File::open(file_path).unwrap_or_else(|err| {
-        eprintln!("ERROR: could not read file {file_path}: {err}");
-        exit(1);
-    });
+#[derive(Debug)]
+pub enum SEError {
+    Io(std::io::Error),
+    Xml(xml::reader::Error),
+}
 
+impl From<io::Error> for SEError {
+    fn from (err: io::Error) -> Self {
+        SEError::Io(err)
+    }
+}
+
+impl From<xml::reader::Error> for SEError {
+    fn from (err: xml::reader::Error) -> Self {
+        SEError::Xml(err)
+    }
+}
+
+fn read_entire_xml_file<P>(file_path: P) -> Result<String, SEError> 
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(file_path)?;
     let er = EventReader::new(file);
     let mut content = String::new();
     for event in er.into_iter() {
-        let event = event.unwrap_or_else(|err| {
-            eprintln!("ERROR: could not read next XML event: {err}");
-            exit(1);
-        });
-
-        match event {
+        match event? {
             XmlEvent::Characters(text) => {
                 content.push_str(&text);
             }
@@ -25,5 +39,22 @@ fn main() {
         }
     }
 
-    println!("{content}");
+    Ok(content)
+}
+
+fn main() -> io::Result<()> {
+    let target_path = "docs.gl/gl4";
+    let dir = fs::read_dir(target_path)?;
+    for file in dir {
+        let file_path = file?.path();
+        let content = read_entire_xml_file(&file_path).unwrap_or_else(|err| {
+            eprintln!("ERROR: could not read file {file_path:?} with {err:?}");
+            exit(1)
+        });
+
+        println!("{file_path:?} -> {size}", size = content.len());
+    }
+
+
+    Ok(())
 }
